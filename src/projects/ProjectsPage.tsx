af0cd3d913cd061473ab0bb4745fc4ab7475251f
Krishna Gawade <krishna.gawade@intelrad.com>
2024-08-05 21:23:52 +0530

    completed till lab 19

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
-import { MOCK_PROJECTS } from "./MockProjects";
+import { Fragment, useEffect, useState } from "react";
+// import { MOCK_PROJECTS } from "./MockProjects";
 import { Project } from "./Project";
 import ProjectList from "./ProjectList";
+import { projectAPI } from "./projectAPI";
 
 function ProjectsPage() {
-    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | undefined>(undefined);
+    const [currentPage, setCurrentPage] = useState(1);
 
     const saveProject = (project: Project) => {
         let updatedProjects = projects.map((p: Project) => {
@@ -14,14 +18,78 @@ function ProjectsPage() {
         setProjects(updatedProjects);
     }
 
+    const handleMoreClick = () => {
+        setCurrentPage((currentPage) => currentPage + 1);
+    }
+
+    useEffect(() => {
+        async function loadProjects() {
+            setLoading(true);
+
+            try {
+                const data = await projectAPI.get(currentPage);
+                setError("");
+                
+                if(currentPage === 1) {
+                    setProjects(data);
+                } else {
+                    setProjects((projects) => [...projects, ...data]);
+                }
+            } catch (error) {
+                if(error instanceof Error) {
+                    setError(error.message);
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        loadProjects();
+    }, [currentPage])
+
     return (
-        <>
+        <Fragment>
             <h1>Projects</h1>
+            {
+                error && (
+                    <div className="row">
+                        <div className="card large error">
+                            <section>
+                                <p>
+                                    <span className="icon-alert inverse"></span>
+                                    {error}
+                                </p>
+                            </section>
+                        </div>
+                    </div>
+                )
+            }
             <ProjectList 
                 projects={projects}
                 onSave={saveProject}
             />
-        </>
+            {
+                !loading && !error && (
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <div className="button-group fluid">
+                                <button className="button default" onClick={handleMoreClick}>
+                                    More...
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+            {
+                loading && (
+                    <div className="center-page">
+                        <span className="spinner primary"></span>
+                        <p>Loading...</p>
+                    </div>
+                )
+            }
+        </Fragment>
     )
     
 }
